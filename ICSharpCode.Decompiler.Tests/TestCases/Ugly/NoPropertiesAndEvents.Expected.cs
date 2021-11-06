ceed7f5192eeb3391bc3b0eb30363a25900476c7
ElektroKill <skullmaster0103@gmail.com>
2021-11-06 12:42:07 +0100

    Merge pull request #2476 from yyjdelete/records
    Fix #2475: Add support for sealed records and records with interface

--- ICSharpCode.Decompiler.Tests/TestCases/Ugly/NoPropertiesAndEvents.Expected.cs
+++ ICSharpCode.Decompiler.Tests/TestCases/Ugly/NoPropertiesAndEvents.Expected.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
 // without restriction, including without limitation the rights to use, copy, modify, merge,
 // publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
 // to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 // INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
 // PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
@@ -44,8 +44,7 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Ugly
 			add {
 				EventHandler eventHandler = this.m_MyEvent;
 				EventHandler eventHandler2;
-				do
-				{
+				do {
 					eventHandler2 = eventHandler;
 					EventHandler value2 = (EventHandler)Delegate.Combine(eventHandler2, value);
 					eventHandler = Interlocked.CompareExchange(ref this.m_MyEvent, value2, eventHandler2);
@@ -57,8 +56,7 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Ugly
 			remove {
 				EventHandler eventHandler = this.m_MyEvent;
 				EventHandler eventHandler2;
-				do
-				{
+				do {
 					eventHandler2 = eventHandler;
 					EventHandler value2 = (EventHandler)Delegate.Remove(eventHandler2, value);
 					eventHandler = Interlocked.CompareExchange(ref this.m_MyEvent, value2, eventHandler2);
